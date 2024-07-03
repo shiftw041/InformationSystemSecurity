f3b064d896c10a7dc247f8db930432f50a33c570
shiftw041 <u202112131@hust.edu.cn>
2024-07-04 03:51:59 +0800

    add zip

--- lab/网安04-U20211212131-邬雪菲/lab2/task5/code/parse.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSE_H
-#define PARSE_H
-
-#include "http-tree.h"
-
-void *Parse_parse(int fd, int reqOnly);
-Body_t *getBody(int *n);
-#endif
--- lab/网安04-U20211212131-邬雪菲/lab3/XSS/cookie1.html
+++
@@ -1,1 +0,0 @@
-<script type="text/javascript">alert(document.cookie);</script>
